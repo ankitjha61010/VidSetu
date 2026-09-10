679f44c13086b4efd94c884fbd666c7c09ab07a1
Abhishek <abhishek@hyperlinkinfosystem.com>
2026-09-10 19:29:40 +0530

    fix: update stream playback provider with fast mobile server endpoints

--- src/services/content/playback/providers/StreamPlaybackProvider.ts
+++ src/services/content/playback/providers/StreamPlaybackProvider.ts
@@ -7,15 +7,18 @@ export class StreamPlaybackProvider {
       return {
         provider: 'FULL_STREAM',
         type: 'iframe',
-        url: `https://vidsrc.me/embed/movie?tmdb=${tmdbId}`,
+        url: `https://vidsrc.cc/v2/embed/movie/${tmdbId}`,
         contentId: String(tmdbId),
         isTrailer: false,
         servers: [
-          { name: 'Server 1 (VidSrc HD)', url: `https://vidsrc.me/embed/movie?tmdb=${tmdbId}` },
+          { name: 'Server 1 (VidSrc CC - Mobile Fast)', url: `https://vidsrc.cc/v2/embed/movie/${tmdbId}` },
           { name: 'Server 2 (VidLink Pro)', url: `https://vidlink.pro/movie/${tmdbId}` },
-          { name: 'Server 3 (AutoEmbed)', url: `https://autoembed.co/movie/tmdb/${tmdbId}` },
-          { name: 'Server 4 (VidSrc.to)', url: `https://vidsrc.to/embed/movie/${tmdbId}` },
-          { name: 'Server 5 (2Embed)', url: `https://www.2embed.cc/embed/${tmdbId}` },
+          { name: 'Server 3 (VidSrc HD)', url: `https://vidsrc.me/embed/movie?tmdb=${tmdbId}` },
+          { name: 'Server 4 (VidSrc IN)', url: `https://vidsrc.in/embed/movie/${tmdbId}` },
+          { name: 'Server 5 (VidSrc XYZ)', url: `https://vidsrc.xyz/embed/movie/${tmdbId}` },
+          { name: 'Server 6 (AutoEmbed)', url: `https://autoembed.co/movie/tmdb/${tmdbId}` },
+          { name: 'Server 7 (SmashyStream)', url: `https://player.smashy.stream/movie/${tmdbId}` },
+          { name: 'Server 8 (Embed.su)', url: `https://embed.su/embed/movie/${tmdbId}` },
         ],
       };
     } else {
@@ -25,17 +28,20 @@ export class StreamPlaybackProvider {
       return {
         provider: 'FULL_STREAM',
         type: 'iframe',
-        url: `https://vidsrc.me/embed/tv?tmdb=${tmdbId}&season=${season}&episode=${episode}`,
+        url: `https://vidsrc.cc/v2/embed/tv/${tmdbId}/${season}/${episode}`,
         contentId: String(tmdbId),
         season,
         episode,
         isTrailer: false,
         servers: [
-          { name: 'Server 1 (VidSrc HD)', url: `https://vidsrc.me/embed/tv?tmdb=${tmdbId}&season=${season}&episode=${episode}` },
+          { name: 'Server 1 (VidSrc CC - Mobile Fast)', url: `https://vidsrc.cc/v2/embed/tv/${tmdbId}/${season}/${episode}` },
           { name: 'Server 2 (VidLink Pro)', url: `https://vidlink.pro/tv/${tmdbId}/${season}/${episode}` },
-          { name: 'Server 3 (AutoEmbed)', url: `https://autoembed.co/tv/tmdb/${tmdbId}-${season}-${episode}` },
-          { name: 'Server 4 (VidSrc.to)', url: `https://vidsrc.to/embed/tv/${tmdbId}/${season}/${episode}` },
-          { name: 'Server 5 (2Embed)', url: `https://www.2embed.cc/embedtv/${tmdbId}&s=${season}&e=${episode}` },
+          { name: 'Server 3 (VidSrc HD)', url: `https://vidsrc.me/embed/tv?tmdb=${tmdbId}&season=${season}&episode=${episode}` },
+          { name: 'Server 4 (VidSrc IN)', url: `https://vidsrc.in/embed/tv/${tmdbId}/${season}/${episode}` },
+          { name: 'Server 5 (VidSrc XYZ)', url: `https://vidsrc.xyz/embed/tv/${tmdbId}/${season}/${episode}` },
+          { name: 'Server 6 (AutoEmbed)', url: `https://autoembed.co/tv/tmdb/${tmdbId}-${season}-${episode}` },
+          { name: 'Server 7 (SmashyStream)', url: `https://player.smashy.stream/tv/${tmdbId}?s=${season}&e=${episode}` },
+          { name: 'Server 8 (Embed.su)', url: `https://embed.su/embed/tv/${tmdbId}/${season}/${episode}` },
         ],
       };
     }
